856055b24918c84c2e12fd04a9139dafe69715f6
MichaelSLaufer <Mephistopheles@Mixls-MacBook-Air.local>
2017-01-29 14:48:26 -0800

    Twitter Analysis

--- TwitterSentiments.py
+++ TwitterSentiments.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 
 # Step 1 - Authenticate
-consumer_key= 'rIl2rqxsRmpHxee3mYOXcc7wF'
-consumer_secret= '8nm0x3JthFvW0TPI8HEUFgwb6V0oeFbsNFmVodGWTjrcCR6CZg'
+consumer_key= 'CONSUMER_KEY_HERE'
+consumer_secret= 'CONSUMER_SECRET_HERE'
 
-access_token='3982977314-unHDPCFPyGxXKOYEeFwnYHaJwwQza05bSYzSSYb'
-access_token_secret='25r3kkk0xzU25nti0CQAm4f0y2S80MYNMrCD8FJtOfLA6'
+access_token='ACCESS_TOKEN_HERE'
+access_token_secret='ACCESS_TOKEN_SECRET_HERE'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,6 +16,11 @@ api = tweepy.API(auth)
 #Step 3 - Retrieve Tweets
 public_tweets = api.search('Trump')
 
+
+
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment 
+#You can decide the sentiment polarity threshold yourself
 
 
 for tweet in public_tweets:
